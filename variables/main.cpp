0011c91a6f3d586413485ef23efbbbffab63eb56
seginbytes <segin.bytes@gmail.com>
2022-02-26 15:22:30 +0530

    using cout nothing much

---
+++ variables/main.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+    using std :: cout;
+    using std :: endl;
+
+int main(void)
+{
+    int slices = 5;
+
+    cout << "you have " << slices << " slices of pizza " << endl;
+}
