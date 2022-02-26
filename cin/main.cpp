727d550c991c01dc0e27528c227bf5e0cc92cf5d
seginbytes <segin.bytes@gmail.com>
2022-02-26 15:46:19 +0530

    using cin nothing much dont bother

---
+++ cin/main.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+    using std :: cin;
+    using std :: cout;
+    using std :: endl;
+
+int main(void)
+{
+    int slices;
+    cout << " how many slices do you have ?" << endl;
+    cin >> slices;
+    
+    cout << "you can share with " << slices - 1 << " people" << endl << "Shareing is careing" << endl;
+}
--- variables/main.cpp
+++ variables/main.cpp
@@ -7,4 +7,4 @@ int main(void)
     int slices = 5; // number of slices ;
 
     cout << "you have " << slices << " slices of pizza " << endl;
-}
+} 
