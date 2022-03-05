75a20702c181dac8668db48061fc7c80ae18ab03
seginbytes <segin.bytes@gmail.com>
2022-03-05 12:31:46 +0530

    setting up power functon

--- power/main.cpp
+++ power/main.cpp
@@ -3,7 +3,28 @@
     using std :: cin;
     using std :: endl;
 
+void power (int base,int exponent);
+
+
 int main (void)
 {
-    //
+    int base;
+    cout << "base =  ";
+    cin  >> base;
+    int exponent;
+    cout << "exponent =  ";
+    cin  >> exponent;
+    power(base,exponent);
+
+}
+
+void power (int base,int exponent)
+{
+    int power = 1 ;
+    for(int i = 1; i < exponent+1; i++)
+    {
+        power = power * base;
+    }
+    
+    cout << power << endl;
 }
