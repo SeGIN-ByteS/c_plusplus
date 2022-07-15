ffd1cc5311fe64cf9fff995160c94a8b8ded43ba
segin-GH <segin.bytes@gmail.com>
2022-07-16 03:36:18 +0530

    .

---
+++ mathStuff/main.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include "math_utils.h"
+
+using std :: cout;
+using std :: endl;
+
+
+int main ()
+{
+    // Rectangle myRectangle;
+    // myRectangle.length = 2;
+    // myRectangle.width = 10;
+    // cout << area(myRectangle.length) << endl;
+
+    cout << pow(3,3) << endl;
+    return 0;
+
+}
