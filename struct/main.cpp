46082c8ff14223dee6b83ffff19967da83545092
segin-GH <segin.bytes@gmail.com>
2022-07-16 03:10:22 +0530

    .

--- struct/main.cpp
+++ struct/main.cpp
@@ -1,29 +1,43 @@
 # include <iostream>
 
 using std :: cout;
+using std :: endl;
 
 struct Rectangle
 {
     double length;
     double width;
-
 };
 
-double area (double length, double width )
+double area (double length, double width)
 {
     return length * width;
+}
+double area (double length)
+{
+    return length * length;
+}
 
+double pow (double base, int pow = 2)
+{
+    int total = 1;
+
+    for (int i = 0; i < pow; i++ )
+    {
+        total = total * base;
+    }
+    return total;
 }
 
 
 int main ()
 {
-    Rectangle myRectangle;
-    
-    myRectangle.length = 10;
-    myRectangle.width = 10;
+    // Rectangle myRectangle;
+    // myRectangle.length = 2;
+    // myRectangle.width = 10;
+    // cout << area(myRectangle.length) << endl;
 
-   cout << area(myRectangle.length,myRectangle.width) << std :: endl;
+    cout << pow(3,3) << endl;
     return 0;
 
 }
